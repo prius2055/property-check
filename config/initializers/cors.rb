654998a3fd7aa69dd91c6ace127603929087ea95
prius2055 <prius2055@gmail.com>
2023-11-30 20:56:45 +0100

    Fix CORS issue

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001'
+    origins '*'
     resource(
       '*',
       headers: :any,
