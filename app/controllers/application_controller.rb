860a88901df36648e239d8ba9ca84407c4cb38a6
prius2055 <prius2055@gmail.com>
2023-11-30 21:41:23 +0100

    update application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::API
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username email password password_confirmation])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[username email password password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username email password password_confirmation admin])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[username email password password_confirmation admin])
   end
 end
 
