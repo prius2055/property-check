121a37021ac1369b29f1a8b72437e6cb7c0ff964
prius2055 <prius2055@gmail.com>
2024-08-30 07:04:57 +0100

    Update user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,6 +5,15 @@ class Api::V1::UsersController < ApplicationController
         @users = User.all
         render json: @users
     end
+
+    def create
+        @user = Property.new(user_params)
+        if @user.save
+            render json: { status: 'Success', message: 'User created successfully' }, status: :created
+        else
+            render json: { errors:@user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
 end
 
     
